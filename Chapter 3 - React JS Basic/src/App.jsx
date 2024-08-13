7120b3a467b706d1097ab0076ca557bc07f53619
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-08-13 07:59:08 +0700

    CH03-05

--- Chapter 3 - React JS Basic/src/App.jsx
+++ Chapter 3 - React JS Basic/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
 
-function WebTitle() {
+function WebTitle(props) {
+  const title = props.title
   return (
-    <h1>Netflix Clone</h1>
+    <h1>{title}</h1>
   )
 }
 
-function Articles() {
+function Article(props) {
+  const title = props.title
+  const description = props.description
   return (
     <article>
-      <h3>Artikel Pertama</h3>
-      <p>Ini adalah artikel yang pertama</p>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </article>
   )
 }
@@ -19,10 +22,11 @@ function App() {
   return (
     <main>
       <header>
-        <WebTitle />
-        <Articles />
-        <Articles />
-        <Articles />
+        <WebTitle title="Netflix Clone" />
+        <Article title="Artikel Pertama" description="Ini adalah deskripsi artikel yang pertama" />
+        <Article title="Artikel Kedua" description="Ini adalah deskripsi artikel yang Kedua" />
+        <Article title="Artikel Ketiga" description="Ini adalah deskripsi artikel yang ketiga" />
+
       </header>
     </main>
   )
