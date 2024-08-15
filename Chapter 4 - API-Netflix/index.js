e653aa873a5ffbe9a0c2c0949149244ade6c8dd3
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-08-15 08:20:55 +0700

    CH04-07

--- Chapter 4 - API-Netflix/index.js
+++ Chapter 4 - API-Netflix/index.js
@@ -7,9 +7,34 @@ app.get("/", (request, response) => {
     response.json({ message: "Heho" })
 })
 
-app.get("/movies", (request, response) => {
+//melihat listing movies
+app.get("/my-movies", (request, response) => {
     console.log("api movie")
     response.json({ data: "movies aiueo" })
+})
+
+//menambah listing movies
+app.post("/my-movies", (request, response) => {
+    console.log("inserting new movies")
+    const data = request.body
+    console.log({ data })
+    response.status(201).json({ message: "success" })
+})
+
+//menghapus movies
+app.delete("/my-movies/:id/:token", (request, response) => {
+    console.log("delete movies...")
+    const { id, token } = request.params
+    console.log({ id, token })
+    reaponse.status(204).json({ message: "success" })
+})
+
+//verifikasi
+app.post("/token", (request, response) => {
+    console.log("creating user token...")
+    const data = request.body
+    console.log({ data })
+    response.status(201).json({ message: "token created" })
 })
 
 app.listen(PORT, () => {
