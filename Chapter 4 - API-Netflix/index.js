506562f6f4ff82f113f1833302ea798f3b04e6d2
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-08-15 08:01:13 +0700

    CH04-05

--- Chapter 4 - API-Netflix/index.js
+++ Chapter 4 - API-Netflix/index.js
@@ -7,6 +7,11 @@ app.get("/", (request, response) => {
     response.json({ message: "Heho" })
 })
 
+app.get("/movies", (request, response) => {
+    console.log("api movie")
+    response.json({ data: "movies aiueo" })
+})
+
 app.listen(PORT, () => {
     console.log("Server API running di PORT : " + PORT)
 })
