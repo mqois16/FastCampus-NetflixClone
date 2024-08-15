87ab0731d2077f26241dba68ae1728d6fc8c63ee
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-08-15 21:20:10 +0700

    CH05-08

--- Chapter 5 - React JS Advance/src/pages/Home.jsx
+++ Chapter 5 - React JS Advance/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { languageAtom } from '../jotai/atoms.js'
 import Layout from '../components/Layout.jsx'
 import { useEffect, useState } from 'react'
 import { apiInstance } from '../utils/apiInstance.js'
+import { motion } from 'framer-motion'
 
 
 function Home() {
@@ -25,24 +26,34 @@ function Home() {
   return (
     <Layout>
       <header>
-        <h1>{language == "en" ? "Watch movies for free!" : "Tonton film gratis!"}</h1>
+        <motion.h1
+          initial={{ x: "100%", opacity: 0 }}
+          animate={{ x: "0%", opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+
+        >{language == "en" ? "Watch movies for free!" : "Tonton film gratis!"}</motion.h1>
         <div className='w-full'>
           <p className='text-center text-[#f1f1f1] bg-[#71b] font-bold text-sm my-3 hover:bg-blue-500 hover:text-red-300'>{movies?.message}</p>
-          <div className='grid grid-cols-2 gap-4'>
+
+          <motion.div
+            initial={{ y: "100%", opacity: 0 }}
+            animate={{ y: "0%", opacity: 1 }}
+            transition={{ duration: 1, ease: "anticipate" }}
+            className='grid grid-cols-2 gap-4'>
             <div className='bg-indigo-500 p-4'>
-              <h1>Hello World!</h1>
+              <h1>Hello World 1!</h1>
             </div>
             <div className='bg-indigo-500 p-4'>
-              <h1>Hello World!</h1>
+              <h1>Hello World 2!</h1>
             </div>
             <div className='bg-indigo-500 p-4'>
-              <h1>Hello World!</h1>
+              <h1>Hello World 3!</h1>
             </div>
             <div className='bg-indigo-500 p-4'>
-              <h1>Hello World!</h1>
+              <h1>Hello World 4!</h1>
             </div>
 
-          </div>
+          </motion.div>
         </div>
 
 
