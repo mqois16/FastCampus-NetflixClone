887a330910141cbb86eda870ac7d06cc9b5929f4
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-08-16 17:40:27 +0700

    CH06-02

---
+++ Chapter [6-7-8-9-10] - Main Project/React-Netflix/src/constants/listCTA.js
@@ -0,0 +1,17 @@
+const LIST_CTA_EN = [
+    {
+        title: "Ready to watch? Enter your email to create or restart your membership.",
+        labelInput: "Email Address",
+        buttonSubmit: "Get Started"
+    }
+]
+
+const LIST_CTA_ID = [
+    {
+        title: "Siap menonton? Masukkan email untuk membuat atau memulai lagi keanggotaanmu.",
+        labelInput: "Alamat Email",
+        buttonSubmit: "Mulai"
+    }
+]
+
+export { LIST_CTA_EN, LIST_CTA_EN }
