370abc40dc15f1662f58ae7d8a9e6f2f742bb483
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-09-17 14:11:06 +0700

    CH09-05

--- Chapter [6-7-8-9-10] - Main Project/API-Netflix/index.js
+++ Chapter [6-7-8-9-10] - Main Project/API-Netflix/index.js
@@ -1,12 +1,23 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
 const { ERR, OK } = require("./utils/response")
 const routes = require("./routes/index.route")
+const mongoose = require("mongoose")
+const { MONGO_URL, API_PORT } = process.env
 
-const PORT = 3002
+const PORT = API_PORT
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect(MONGO_URL).catch(err => {
+    if (err) {
+        console.log("mongodb tidak terhubung")
+        throw err
+    }
+    console.log("mongodb terhubung")
+})
 
 app.use(routes)
 
---
+++ Chapter [6-7-8-9-10] - Main Project/API-Netflix/models/index.model.js
@@ -0,0 +1,18 @@
+const { default: mongoose, Schema } = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+    email: {
+        required: true,
+        unique: true,
+        type: String
+    },
+    token: {
+        required: true,
+        type: String
+    },
+    favoriteMovies: Array
+})
+
+module.exports = {
+    User: mongoose.model("User", UserSchema)
+}
